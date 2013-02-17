11df53d19148dcf9402961c8fa2c462320525338
Marcos Alarcon Rivas <kernelpanic0x00@gmail.com>
2013-02-17 15:56:45 +0100

    Small changes

--- src/DoubleValue/Function1.java
+++ src/DoubleValue/Function1.java
@@ -11,9 +11,6 @@ public class Function1 implements Fitness {
 	}
 	
 	@Override
-	/**
-	 * 2·e^x - 3·pi·x² + 12/x
-	 */
 	public double evaluate(Individual creature) {
 		double gen= ((DoubleValue) creature).genotype;
 		return 2*Math.E*gen-3*Math.PI*gen*gen+12/gen;
@@ -22,6 +19,10 @@ public class Function1 implements Fitness {
 	@Override
 	public boolean maximization() {
 		return true;
+	}
+	
+	public String toString(){
+		return "2·e^x -3·pi·x² +12/x";
 	}
 
 
