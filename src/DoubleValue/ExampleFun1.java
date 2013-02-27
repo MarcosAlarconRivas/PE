25b07d970b044f53d21841ba2edbe4048c7aaf85
Marcos Alarcon Rivas <kernelpanic0x00@gmail.com>
2013-02-27 18:36:27 +0100

    Updated ExampleFun1

--- src/DoubleValue/ExampleFun1.java
+++ src/DoubleValue/ExampleFun1.java
@@ -2,6 +2,11 @@ package DoubleValue;
 
 public class ExampleFun1 extends DoubleFunction{
 	
+	protected static double lowLimit[]= {-1};
+	protected static double highLimit[]={ 5};
+	protected static int numOfChromosomes = 1;
+	protected static boolean maximization = true;
+	
 	public String toString(){
 		return "2·e^x -3·pi·x² +12/x";
 	}
