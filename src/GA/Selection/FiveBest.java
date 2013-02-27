7a0c4ad9c460fce87b6810d552395ff1976a94bd
mario800ml <marioarranz@hotmail.com>
2013-02-27 16:05:03 +0100

    FiveBest added

---
+++ src/GA/Selection/FiveBest.java
@@ -0,0 +1,16 @@
+package GA.Selection;
+
+import GA.Population;
+
+public class FiveBest implements Selection {
+
+	@Override
+	public int[] select(Population pop) {
+		int h[] = new int [5];
+		for()
+			pop.people[0].fitness();
+			h[i]=0;
+		return null;
+	}
+
+}
