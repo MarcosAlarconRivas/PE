a86f1ef585bfb4a41b1c6bb26897da91a06bbfba
Marcos Alarcon Rivas <kernelpanic0x00@gmail.com>
2013-03-06 16:26:51 +0100

    DoubleChromosome methods implemetation

--- src/DoubleValue/DoubleChromosome.java
+++ src/DoubleValue/DoubleChromosome.java
@@ -1,12 +1,16 @@
 package DoubleValue;
 
 import java.util.LinkedList;
-import java.util.List;
 import Utilities.BitVector;
 
 public class DoubleChromosome extends BitVector {
-	DoubleChromosome(int length){
+	
+	DoubleChromosome(long length){
 		super(RANDOM, length);
+	}
+	
+	DoubleChromosome(long length, boolean setValue){
+		super(setValue?ALL_1:ALL_0, length);
 	}
 	
 	/**
@@ -15,7 +19,7 @@ public class DoubleChromosome extends BitVector {
 	 * @return the bit vector parts as boolean[]
 	 */
 	boolean [][] meiosis(long cutPoints[]){
-		//FIXME read it carefully &/or test it
+		//FIXME debug
 		boolean result[][] =  new boolean [cutPoints.length+1][];
 		long head = 0;
 		for (int p = 0; p<= cutPoints.length; head=cutPoints[p++]){
@@ -30,11 +34,39 @@ public class DoubleChromosome extends BitVector {
 	/**
 	 * Rebuilds chromosomes from meiosis results.
 	 */
-	public static LinkedList<DoubleChromosome> rebuid(List<boolean [][]> dna){
-		LinkedList<DoubleChromosome> reslut = new LinkedList<DoubleChromosome>();
-		int numOfChilds = dna.size();//same as numOfParents
+	public static LinkedList<DoubleChromosome> rebuid(boolean [][][] dna){
+		//FIXME debug
+		LinkedList<DoubleChromosome> result = new LinkedList<DoubleChromosome>();
 		
-		return reslut;
+		long total_length = 0;
+		int numOfCuts= dna [0][0].length;
+		int numOfParents = dna.length;
 		
+		for(int i=0;i<dna[0].length;i++)
+			total_length += dna[0][i].length;
+
+		for(int child=0; child<numOfParents; child++){
+			DoubleChromosome newC = new DoubleChromosome(total_length, false);
+			long bit = 0;
+			for(int cut=0; cut<numOfCuts; cut++){
+				bit=newC.paste(dna[(child+cut)%numOfParents][(child+cut)%numOfCuts], bit);
+			}
+			result.add(newC);
+		}	
+		return result;
+	}
+	
+	/**
+	 * Copies a boolean vector in given position and nexts
+	 * @param bits_to_copy
+	 * @param begin_bit
+	 * @return begin_bit + bits_to_copy.length (how long has been modified the vector)
+	 */
+	private long paste(boolean[] bits_to_copy, long begin_bit) {
+		
+		for (int bit =0; bit<bits_to_copy.length; bit++)
+			set(bit+begin_bit, bits_to_copy[bit]);
+		
+		return begin_bit + bits_to_copy.length;
 	}
 }
