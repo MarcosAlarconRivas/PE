22067cb528876552942755a0d7c48bee54f36979
Marcos Alarcon Rivas <kernelpanic0x00@gmail.com>
2013-03-14 00:16:10 +0100

    Implemeted OnePieceOfEach (crossover)

--- src/DoubleValue/OnePieceOfEach.java
+++ src/DoubleValue/OnePieceOfEach.java
@@ -1,5 +1,6 @@
 package DoubleValue;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -21,11 +22,10 @@ public class OnePieceOfEach extends VarParentsCross implements Crossover {
 		@SuppressWarnings("static-access")
 		int genotypeBits[] = ((DoubleFunction)(pop.getFitnessFunction())).genotypeBits;
 		
-		for( int i=0; i<parent.length-1; i+=2){
-			//generar nueva lista de parents
+		for( int i=0; i<parent.length-numOfParents; ){
 			LinkedList<DoubleValue> parents = new LinkedList<DoubleValue>();
 			for (int j=0; j<numOfParents; j++)
-				parents.add((DoubleValue) pop.people[parent[j]]);
+				parents.add((DoubleValue) pop.people[parent[i++]]);
 			cross(childsList, genotypeBits, parents);
 		}
 		return childsList;
@@ -34,6 +34,35 @@ public class OnePieceOfEach extends VarParentsCross implements Crossover {
 	private void cross(LinkedList<Individual> childs, int[] genotypeBits, 
 			List<DoubleValue> parents) 
 	{
-		//TODO see same method in SinglePiontCut
+		DoubleChromosome[][] childs_dna = new DoubleChromosome[numOfParents][genotypeBits.length];
+			//numOfParents = numOfChids = numOfCuts+1
+		
+		for(int chr=0; chr<genotypeBits.length; chr++){//for each chromosome
+			
+			//generate cut points
+			long[] cutPoints = new long[numOfParents-1];
+			for(int cut= 0; cut<numOfParents-1; cut++)
+				cutPoints[cut]= (long)Math.floor(Math.random()*genotypeBits[chr]);
+			Arrays.sort(cutPoints);//order chosen cut points
+			
+			//cut parents chromosomes
+			boolean parents_dna[][][] = new boolean[numOfParents][][];
+			int p= 0;
+			for(DoubleValue parent: parents)
+				parents_dna[p++] = parent.genotype[chr].meiosis(cutPoints);
+			if(p!=numOfParents)
+				System.out.println("Error at OnePieceOfEach: private void cross\n numOfParents should be also the size of @param List<DoubleValue> parents");
+			
+			//set childs new chromosomes
+			List<DoubleChromosome> mix = DoubleChromosome.rebuild(parents_dna);
+			p= 0;
+			for (DoubleChromosome chrom: mix)
+				childs_dna[p++][chr] = chrom;
+			if(p!=numOfParents)
+				System.out.println("Error at OnePieceOfEach: private void cross\n child num check failed");
+		}
+		//create new individuals using the generated chromosomes
+		for(int child= 0; child<numOfParents; child++)
+			childs.add(new DoubleValue(childs_dna[child]));
 	}
 }
