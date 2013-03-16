2808ed41955e17e7f890f7a5eee422294fe74a38
Marcos Alarcon Rivas <kernelpanic0x00@gmail.com>
2013-03-16 13:20:16 +0100

    SurvivalOfTheFittest implemeted

--- src/ga/replacement/SurvivalOfTheFittest.java
+++ src/ga/replacement/SurvivalOfTheFittest.java
@@ -1,19 +1,24 @@
 package ga.replacement;
 
+import java.util.Arrays;
 import java.util.List;
 
 import ga.Individual;
 import ga.Population;
-
+/**
+ * Supposing the population is ordered, 
+ * replaces worst individual with children (if they are better).
+ */
 public class SurvivalOfTheFittest implements Replacement {
 
 	@Override
-	public void replace(List<? extends Individual> children, int[] parents,
-			Population pop) {
-		
+	public void replace(List<? extends Individual> children, int[] parents, Population pop) {
+		Individual c[]=(Individual[])children.toArray();
+		Arrays.sort(c);
+		for(int i=c.length, j=c.length; j>=0; j--)
+			if(pop.people[j].compareTo(c[i])>0)
+				pop.people[j]=c[i--];
 		
 	}
-
-
 
 }
