2bdadac4f7f280a79601963925da3b81f8454f27
Marcos Alarcon Rivas <kernelpanic0x00@gmail.com>
2013-03-20 19:09:19 +0100

    nothing important

--- src/p1/Function1.java
+++ src/p1/Function1.java
@@ -14,9 +14,8 @@ public class Function1 extends DoubleFunction{
 	@Override
 	protected double evaluate(double[] fenotypes) {
 		double x = fenotypes[0];
-		double r1 = Math.exp(-0.2*Math.abs(x));
-		double r2 = Math.exp( Math.cos(2*Math.PI*x));
-		System.out.println(x+"->"+r1+" "+r2);//XXX
-		return 20+ -20*Math.E*r1  -r2;
+		double r1 = -0.2*Math.abs(x);
+		double r2 = Math.cos(2*Math.PI*x);
+		return 20+ -20*Math.E* Math.exp(r1)  -Math.exp(r2);
 	}
 }
