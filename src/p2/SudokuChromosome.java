e9d120be2947fd6e9faad00cf61832af448da477
Marcos Alarcon Rivas <kernelpanic0x00@gmail.com>
2013-04-15 18:04:22 +0200

    Basic chromosome structure

--- src/p2/SudokuChromosome.java
+++ src/p2/SudokuChromosome.java
@@ -2,6 +2,36 @@ package p2;
 
 
 public class SudokuChromosome {
+	protected int rows[][] = new int[9][9];
 	
+
+	public int[] getRow(int r){
+		return rows[r].clone();
+	}
+	
+	public int[] getCol(int c){
+		int[] column = new int[9];
+		
+		for(int r=0;r<9;r++)
+			column[r] = rows[r][c];
+			
+		return column;
+	}
+	
+	/**
+	 *  ________
+	 * |0 |1 |2 |
+	 * |3 |4 |5 |
+	 * |6 |7 |8 |
+	 */
+	public int[] getblk(int b){
+		int[] block = new int[9];
+		
+		for(int r=0; r<3; r++)
+			for(int c=0; c<3; c++)
+				block[r+c]= rows[b/3+r][b%3+c];
+		
+		return block;
+	}
 
 }
