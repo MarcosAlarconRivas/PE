702cdb1cce2c30b8c216169d186ec52ac64f1d42
Marcos Alarcon Rivas <kernelpanic0x00@gmail.com>
2013-04-16 16:18:28 +0200

    Simplification of chromosome

--- src/p2/Conflicts.java
+++ src/p2/Conflicts.java
@@ -1,10 +1,33 @@
 package p2;
 
 import ga.Fitness;
+import ga.Individual;
 
-public abstract class Conflicts implements Fitness {
+public class Conflicts implements Fitness {
+	
+	//Singleton instance
+	private static Conflicts instance= new Conflicts();
+	
+	private Conflicts(){
+		super();
+	}
 	
 	@Override
 	public boolean maximization(){return false;}
+
+	public static Fitness getInstance() {
+		return instance;
+	}
+
+	@Override
+	public Class<? extends Individual> specie() {
+		return Sudoku.class;
+	}
+
+	@Override
+	public double evaluate(Individual creature) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
