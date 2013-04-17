92ba452ccfadcf10c8aba9fc8fa799224ab3419b
Marcos Alarcon Rivas <kernelpanic0x00@gmail.com>
2013-04-17 11:53:20 +0200

    First code for ChooseOne (mutation)

--- src/p2/ChooseOne.java
+++ src/p2/ChooseOne.java
@@ -1,20 +1,46 @@
 package p2;
 
+import java.util.Random;
+
 import ga.Individual;
 import ga.Mutation;
 /**
- * This mutation tries 3 times to change two positions of the same row.
- * returns the best one of the 3
+ * This mutation tries 'numOfTries' times to change two positions of the same row.
+ * returns the best one of them.
  */
 public class ChooseOne extends Mutation {
+	
+	public static int numOfTries= 3;
 
 	public ChooseOne(double baseProb, double inbreedingControl, double annealingControl) {
 		super(baseProb, inbreedingControl, annealingControl);
 	}
 
 	@Override
-	protected void mutate(Individual i) {
-		// TODO Auto-generated method stub
+	protected void mutate(Individual sudoku) {
+		Random r = new Random();
+		
+		for(int i=0; i<numOfTries; i++){
+			//choose a row
+			int row = r.nextInt(9);
+			
+			//choose 2 cols
+			int c1= r.nextInt(9);
+			int c2 = r.nextInt(9);
+			
+			if(c1==c2 |c1<0 || c2<0) break;
+			
+			Sudoku newOne  = (Sudoku)sudoku.clone();
+			int p = newOne.rows[row][c1];
+			newOne.rows[row][c1]= newOne.rows[row][c2];
+			newOne.rows[row][c2]= p;
+			
+			//FIXME evaluate newOne ?
+			
+			if(newOne.compareTo(sudoku)>0)//choose the best one
+				sudoku= newOne;//FIXME ref arg?
+		
+		}
 
 	}
 
--- src/p2/Sudoku.java
+++ src/p2/Sudoku.java
@@ -3,16 +3,54 @@ package p2;
 import ga.Individual;
 /**
  * Individual representing a Sudoku
+ * Negative values represents input data (positions that can't change).
  */
 public class Sudoku extends Individual {
 
 	protected int rows[][] = new int[9][9];
 	
+	/** new empty sudoku**/
+	Sudoku(){
+		super(Conflicts.getInstance());
+	}
+	
+	/**
+	 * Creates a new random sudoku with the fixed positions in rows.
+	 */
+	public Sudoku(int rowsMatrix[][]){
+		super(Conflicts.getInstance());
+		for(int i=0; i<9; i++)
+			rows[i]= generateRow(rowsMatrix[i]);
+		
+	}
+	
+	/**
+	 * Generate a valid row using the given positions
+	 */
+	private int[] generateRow(int[] input) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
+	/**
+	 * 0->[___][___][___]
+	 * 1->[...
+	 * 2->
+	 * ...
+	 * 8->[___][___][___]
+	 */
 	public int[] getRow(int r){
 		return rows[r].clone();
 	}
 	
+	/**
+	 * 0 1 2 | 3 4 5 | 6 7 8
+	 * | | |   | | |   | | |
+	 * v v v   v v v   v v v 
+	 * [][][]| [][][]| [][][]
+	 * [][][]| ......| ......
+	 * .....
+	 */
 	public int[] getCol(int c){
 		int[] column = new int[9];
 		
@@ -44,6 +82,5 @@ public class Sudoku extends Individual {
 		copy.rows = rows.clone();
 		return copy;
 	}
-
 	
 }
