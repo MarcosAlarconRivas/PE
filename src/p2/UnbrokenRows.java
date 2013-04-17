ed260f4f3b01444049cb85b1d0b671bab5704649
Marcos Alarcon Rivas <kernelpanic0x00@gmail.com>
2013-04-17 15:31:38 +0200

    Debugged ChooseOne to make always mutate

--- src/p2/UnbrokenRows.java
+++ src/p2/UnbrokenRows.java
@@ -11,6 +11,8 @@ import ga.VarParentsCross;
  */
 public class UnbrokenRows extends VarParentsCross implements Crossover {
 	
+	protected static int numOfParents = 3;
+	
 	@Override
 	public void setNumOfParents(int num){
 		if(num<2)
@@ -25,7 +27,27 @@ public class UnbrokenRows extends VarParentsCross implements Crossover {
 	public LinkedList<Individual> crossover(int[] parents, Population pop) {
 		LinkedList<Individual> children = new LinkedList<Individual> ();
 		
-		return null;
+		for(int i=0; i<parents.length; i+=numOfParents){
+			addChildrenNewChildren(children, pop, parents, i);
+		}
+		
+		return children;
+	}
+
+	private void addChildrenNewChildren(LinkedList<Individual> children, Population pop, 
+			int[] parents, int firstPar) {
+		
+		for(int child=0; child<numOfParents; child++){
+			//generate new child
+			Sudoku newChild = new Sudoku();
+			int row;
+			for(int par=child; ; par= (par+1)%numOfParents){
+				
+			}
+			
+			
+			//add in children
+		}
 	}
 
 }
