1c9a3f48792f7d2cf9b0fca220de6340478390ea
Marcos Alarcon Rivas <kernelpanic0x00@gmail.com>
2013-05-09 21:49:15 +0200

    Expreimental code

--- src/p3/MUX4.java
+++ src/p3/MUX4.java
@@ -1,5 +1,7 @@
 package p3;
 
+import java.util.LinkedList;
+
 import ga.Fitness;
 import ga.Individual;
 
@@ -31,9 +33,42 @@ public class MUX4 implements Fitness {
 	public boolean maximization() {
 		return true;
 	}
+
+	@SuppressWarnings({ "unchecked", "unused" })
+	private static LinkedList<LinkedList<Boolean>>testingList(int ln) {
+		if(ln<0)return null;
+		if(ln==0)
+			return new LinkedList<LinkedList<Boolean>>();
+		LinkedList<LinkedList<Boolean>> result= new LinkedList<LinkedList<Boolean>>();
+		if(ln==1){
+			LinkedList<Boolean> t, f;
+			t= new LinkedList<Boolean>();
+			t.add(true);
+			result.add(t);
+			f= new LinkedList<Boolean>();
+			t.add(false);
+			result.add(f);
+			return result;
+		}
+		
+		LinkedList<LinkedList<Boolean>> t, f;
+		f = testingList(ln-1);
+		t = (LinkedList<LinkedList<Boolean>>)f.clone();
+
+		for(LinkedList<Boolean> l: f){
+			l.addLast(false);
+			result.addLast(l);
+		}
+		
+		for(LinkedList<Boolean> l: t){
+			l.addLast(true);
+			result.addLast(l);
+		}
+		
+		return result;
+	}
 	
 	private static boolean[][] testingVector() {
-		//TODO generate it with a loop
 		return new boolean[][]{
 		{false,false,false,false,false,false},
 		{false,false,false,false,false,true},
@@ -100,6 +135,15 @@ public class MUX4 implements Fitness {
 		{true,true,true,true,true,false},
 		{true,true,true,true,true,true}
 		};
+	}
+	
+	public static void main(String args[]){
+		for(int i=0; i<test.length; i++){
+			for(int j=0; j<test[i].length; j++)
+				System.out.print(test[i][j]?1:0);
+			System.out.println();
+		}
+			
 	}
 
 
